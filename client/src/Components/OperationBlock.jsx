73eb7d551b658a7931531ca16f243ca6dc1e40db
Ilya Osipov <ilyaoa19@gmail.com>
2024-12-10 22:13:26 +0300

    minorVisualDnD

--- client/src/Components/OperationBlock.jsx
+++ client/src/Components/OperationBlock.jsx
@@ -1,30 +1,34 @@
 import { useDrag } from "react-dnd";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { Operation } from "../data/Operation/model";
 
 export default function OperationBlock({ operation }) {
-    const [, drag] = useDrag(() => ({
-        type: "BLOCK",
-        item: operation,
-    }));
-    
-    return (
-        <div
-            ref={drag}
-            style={{
-                padding: "10px",
-                margin: "5px 0",
-                backgroundColor: "#FFAD84",
-                color: 'black',
-                borderRadius: 10,
-                cursor: "grab",
-            }}
-        >
-            {operation.name}
-        </div>
-    );
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "BLOCK",
+    item: operation,
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }));
+
+  return (
+    <div
+      ref={drag}
+      draggable={false}
+      style={{
+        padding: "10px",
+        margin: "5px 0",
+        backgroundColor: isDragging ? "#FFAD55" : "#FFAD84",
+        color: "black",
+        borderRadius: 10,
+        cursor: "grab",
+      }}
+    >
+      {operation.name}
+    </div>
+  );
 }
 
 OperationBlock.propTypes = {
-    operation: PropTypes.instanceOf(Operation).isRequired,
+  operation: PropTypes.instanceOf(Operation).isRequired,
 };
