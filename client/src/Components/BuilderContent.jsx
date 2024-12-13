cdf34c8cdfe8ac7b7aac651472a02e3c2c73502a
Ilya Osipov <ilyaoa19@gmail.com>
2024-12-13 05:51:11 +0300

    added test clear v.1

--- client/src/Components/BuilderContent.jsx
+++ client/src/Components/BuilderContent.jsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 import { replaceParams } from "../tools";
 import { useLocation, useNavigate, useParams } from "react-router";
 import { analyzeFormula } from "../data/Formula/actions";
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 import { deleteMyNodeByIndex } from "../data/Node/slice";
 import { patchUserNodeByIndex } from "../data/Node/actions";
 import { Operation } from "../data/Operation/model";
@@ -16,64 +16,91 @@ const { Header, Content } = Layout;
 const { Title, Text } = Typography;
 
 export default function BuilderContent() {
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
   const [operation, setOperation] = useState();
-  const { myNodes } = useSelector(state => state.node)  
-  const { operations } = useSelector(state => state.operation)  
+  const { myNodes } = useSelector((state) => state.node);
+  const { operations } = useSelector((state) => state.operation);
 
-  const { nodeId = '0' } = useParams(); // Получаем ID ноды из URL
+  const { nodeId = "0" } = useParams(); // Получаем ID ноды из URL
   const navigate = useNavigate();
   const location = useLocation();
-  
+
   const [node, setNode] = useState({});
   const [params, setParams] = useState([]);
   const [isLegendModalVisible, setIsLegendModalVisible] = useState(false);
-  
+
   useEffect(() => {
     if (nodeId) {
-      const id = Number(nodeId)
-      const userNode = myNodes?.[id]
+      const id = Number(nodeId);
+      const userNode = myNodes?.[id];
       if (userNode) {
-        const paramsTree = myNodes[id].params ? myNodes[id].params.map((param) => typeof param === 'number' ? myNodes[param] : param) : [];
-        
-        setNode({...myNodes[id], params: paramsTree});
+        const paramsTree = myNodes[id].params
+          ? myNodes[id].params.map((param) =>
+              typeof param === "number" ? myNodes[param] : param
+            )
+          : [];
+
+        setNode({ ...myNodes[id], params: paramsTree });
       } else {
-        navigate(-1) // нету ноды ? редирект назад
+        navigate(-1); // нету ноды ? редирект назад
       }
     } else {
       setNode(myNodes[0]);
     }
-  }, [dispatch, myNodes, navigate, nodeId])
+  }, [dispatch, myNodes, navigate, nodeId]);
 
   useEffect(() => {
-    const id = Number(nodeId)
+    const id = Number(nodeId);
     if (operations.length > 0) {
-      const myNodeOperation = operations.find(op => myNodes[id].operation === op.id)
+      const myNodeOperation = operations.find(
+        (op) => myNodes[id].operation === op.id
+      );
       if (myNodeOperation) {
-        setOperation(new Operation(myNodeOperation))
-        setParams(node.params.map((param) => typeof param === 'object' ? param.operation && param.params ? replaceParams(operations, param) : '' : param));
+        setOperation(new Operation(myNodeOperation));
+        setParams(
+          node.params.map((param) =>
+            typeof param === "object"
+              ? param.operation && param.params
+                ? replaceParams(operations, param)
+                : ""
+              : param
+          )
+        );
       } else {
-        setOperation()
+        setOperation();
       }
     }
-  }, [myNodes, node.params, nodeId, operations])  
+  }, [myNodes, node.params, nodeId, operations]);
+
+  useEffect(() => {
+    console.log("Current operation:", operation);
+    console.log("Node params:", node.params);
+  }, [operation, node.params]);
 
   function cancel() {
-    const id = Number(nodeId)
+    const id = Number(nodeId);
     for (let i = 0; i < myNodes.length; i++) {
-      const nodeIndexWithDependency = myNodes.findIndex((node) => node.params && node.params.some((param) => param === id))
+      const nodeIndexWithDependency = myNodes.findIndex(
+        (node) => node.params && node.params.some((param) => param === id)
+      );
       if (nodeIndexWithDependency >= 0) {
-        patchUserNodeByIndex(
-          dispatch,
-          nodeIndexWithDependency,
-          { 
-            params: myNodes[nodeIndexWithDependency].params.map((param) => param === id ? 'x' : param)
-          }
-        )
+        patchUserNodeByIndex(dispatch, nodeIndexWithDependency, {
+          params: myNodes[nodeIndexWithDependency].params.map((param) =>
+            param === id ? "x" : param
+          ),
+        });
       }
     }
     deleteMyNodeByIndex(id);
     navigate(-1);
+  }
+
+  function clear() {
+    if (nodeId !== "0") {
+      navigate(-1);
+    } else {
+      navigate(0);
+    }
   }
 
   function save() {
@@ -86,20 +113,45 @@ export default function BuilderContent() {
 
   const expression = useMemo(() => {
     if (operations.length > 0 && node.params && node.operation) {
-      return replaceParams(operations, node)
+      return replaceParams(operations, node);
     }
-    return ''
-  }, [operations, node])
+    return "";
+  }, [operations, node]);
+
+  console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+  console.log("Node:", node);
+  console.log("Params:", params);
+  console.log("Operations:", operations);
+  console.log("MyNodes:", myNodes);
+  console.log("Node ID:", nodeId);
+  console.log("Node Params:", node.params);
+  console.log("MyOper", myNodes[nodeId]);
+  console.log("Expression", expression);
+  console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+
+  console.log("Generated expression:", expression);
 
   return (
     <Layout style={{ display: "flex", flexDirection: "column" }}>
       <Header style={{ backgroundColor: "#fff", padding: "10px 20px" }}>
         <Title level={3}>Конструктор формул</Title>
       </Header>
-      <LegendModal visible={isLegendModalVisible} onClose={() => setIsLegendModalVisible(false)} onSave={() => {}} legendResult={<BlockMath math={expression} />} />
-      <Content style={{ padding: "20px", overflow: "auto", backgroundColor: "#fff"}}>
+      <LegendModal
+        visible={isLegendModalVisible}
+        onClose={() => setIsLegendModalVisible(false)}
+        onSave={() => {}}
+        legendResult={<BlockMath math={expression} />}
+      />
+      <Content
+        style={{ padding: "20px", overflow: "auto", backgroundColor: "#fff" }}
+      >
         <div>
-          <Title level={4}>{location.state ? `Формула для параметра ${location.state}` : 'Основная формула'}</Title>
+          <Title level={4}>
+            {location.state
+              ? `Формула для параметра ${location.state}`
+              : "Основная формула"}
+          </Title>
+          {console.log(location.state, "this is location.state")}
           <div
             style={{
               border: "1px solid #d9d9d9",
@@ -108,24 +160,49 @@ export default function BuilderContent() {
               marginBottom: "20px",
             }}
           >
-            {operations.length > 0 && node.params && node.operation ? 
-              <BlockMath math={expression} /> : 
+            {operations.length > 0 && node.params && node.operation ? (
+              <BlockMath math={expression} />
+            ) : (
               <Text type="secondary">*формула в формате текста*</Text>
-            }
+            )}
           </div>
-
-          <EditArea nodeId={nodeId} operation={operation} setOperation={setOperation} params={params} setParams={setParams} />
-          
+          <EditArea
+            nodeId={nodeId}
+            operation={operation}
+            setOperation={setOperation}
+            params={params}
+            setParams={setParams}
+          />
           <div>
-            {nodeId !== '0' && <Button style={{ color: 'red', marginTop: '8px' }} onClick={cancel}>
-              Отменить
-            </Button>}
+            {nodeId !== "0" && (
+              <Button
+                style={{ color: "red", marginTop: "8px" }}
+                onClick={cancel}
+              >
+                Отменить
+              </Button>
+            )}
 
-            {nodeId !== '0' && <Button style={{ color: 'green', marginTop: '8px' }} onClick={save}>
-              Сохранить
-            </Button>}
+            {nodeId !== "0" && (
+              <Button
+                style={{ color: "green", marginTop: "8px" }}
+                onClick={save}
+              >
+                Сохранить
+              </Button>
+            )}
           </div>
-
+          <Button
+            style={{
+              color: "red",
+              marginTop: "8px",
+              marginLeft: "auto",
+              display: "block",
+            }}
+            onClick={clear}
+          >
+            Очистить
+          </Button>
 
           <div style={{ marginTop: "20px" }}>
             <Input.TextArea
@@ -136,13 +213,23 @@ export default function BuilderContent() {
             />
           </div>
         </div>
-        <Button style={{ color: 'blue', marginTop: '8px' }} onClick={() => {analyzeFormula(dispatch)}}>
+        <Button
+          style={{ color: "blue", marginTop: "8px" }}
+          onClick={() => {
+            analyzeFormula(dispatch);
+          }}
+        >
           Найти совпадения
         </Button>
-        {Boolean(expression.length) && <Button style={{ color: 'green', margin: '8px 0 0 5px' }} onClick={() => setIsLegendModalVisible(true)}>
-          Сохранить формулу
-        </Button>}
+        {Boolean(expression.length) && (
+          <Button
+            style={{ color: "green", margin: "8px 0 0 5px" }}
+            onClick={() => setIsLegendModalVisible(true)}
+          >
+            Сохранить формулу
+          </Button>
+        )}
       </Content>
-  </Layout>
-  )
+    </Layout>
+  );
 }
--- client/src/Components/EditArea.jsx
+++ client/src/Components/EditArea.jsx
@@ -4,138 +4,189 @@ import PropTypes from "prop-types";
 import { Operation } from "../data/Operation/model";
 import { useEffect, useState } from "react";
 const { Text } = Typography;
-import { Input, Button } from 'antd';
+import { Input, Button } from "antd";
 import { Link, useNavigate } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { addUserNode, patchUserNodeByIndex } from "../data/Node/actions";
 
-export default function EditArea({ nodeId, operation, setOperation, params, setParams }) {
-    const navigate = useNavigate();
-    const { myNodes } = useSelector(state => state.node);
-    const [isDropped, setIsDropped] = useState(false);
+export default function EditArea({
+  nodeId,
+  operation,
+  setOperation,
+  params,
+  setParams,
+}) {
+  const navigate = useNavigate();
+  const { myNodes } = useSelector((state) => state.node);
+  const [isDropped, setIsDropped] = useState(false);
 
-    const [, drop] = useDrop(() => ({
-        accept: "BLOCK",
-        drop: (item) => {       
-            setIsDropped(true)              
-            setOperation(item);
-        },
-    }));
+  const [, drop] = useDrop(() => ({
+    accept: "BLOCK",
+    drop: (item) => {
+      setIsDropped(true);
+      setOperation(item);
+    },
+  }));
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const id = Number(nodeId || 0)
+  useEffect(() => {
+    const id = Number(nodeId || 0);
 
-        if (operation && !myNodes[id].operation && isDropped) {
-            setParams(operation.params)
-            patchUserNodeByIndex(dispatch, id, { 
-                operation: operation.id,
-                params: operation.params,
-            })
-        }
-    }, [dispatch, isDropped, myNodes, nodeId, operation, setParams])
-    
-    function handleInputChange(value, paramIndex) {
-        const id = Number(nodeId || 0)
-        setParams((prev) =>
-            prev.map((param, index) =>
-                index === paramIndex ? value : param
-            )
-        );
-        patchUserNodeByIndex(dispatch, id, { 
-            params: myNodes[id].params?.map((p, index) => index === paramIndex ? value : p) || []
-        })
+    if (operation && !myNodes[id].operation && isDropped) {
+      setParams(operation.params);
+      patchUserNodeByIndex(dispatch, id, {
+        operation: operation.id,
+        params: operation.params,
+      });
     }
+  }, [dispatch, isDropped, myNodes, nodeId, operation, setParams]);
+  console.log(nodeId, 'NODE ID')
+  console.log(operation, 'OPERATION')
 
-    function handleChangeMode(param, paramIndex) {        
-        const id = Number(nodeId || 0)
-        setIsDropped(false)
-        addUserNode(dispatch, { id: myNodes.length })
-        patchUserNodeByIndex(dispatch, id, { 
-            params: myNodes[id].params?.map((p, index) => index === paramIndex ? myNodes.length : p) || [] // сетим myNodes.length, как id следующей ноды
-        })
-        setOperation()
-        setParams([])
-        navigate('/' + myNodes.length, { state: param }) // редиректим на страницу, param нужен для тайтла
-    }
-
-    return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 2,
-        }}>
-            <Text type="secondary">{operation?.name}</Text>
-            <div
-                ref={drop}
-                style={{
-                minHeight: "150px",
-                display: 'flex',
-                flex: 1,
-                border: "2px dashed #d9d9d9",
-                borderRadius: "4px",
-                padding: "10px",
-                textAlign: "center",
-                }}
-            >
-                <Text type="secondary">
-                    {!operation && "Перетаскивайте блоки для построения формулы"}
-                </Text>
-                {params?.length > 0 && operation && (
-                    <div style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        justifyContent: 'space-evenly',
-                        gap: 20,
-                        flexWrap: "wrap",
-                    }}>
-                        {params.map((param, index) => (
-                            <div  key={index}  style={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                gap: 5,
-                                alignItems: 'start',
-                            }}>
-                                {/* если параметр это реф на другую ноду, то отображаем кнопку, иначе это инпут */}
-                                {typeof myNodes[Number(nodeId)]?.params?.[index] === 'number' ?
-                                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                                            <span style={{ backgroundColor: '#FAFAFA', border: '1px solid #D9D9D9', padding: '0 11px', lineHeight: 1, borderStartStartRadius: 6, borderEndStartRadius: 6, height: 30, display: 'flex', alignItems: 'center', fontSize: 14 }}>
-                                                x
-                                            </span>
-                                            <Link to={`/${myNodes[Number(nodeId)].params[index]}`} style={{ borderLeft: 'none', textDecoration: 'none', border: '1px solid #D9D9D9', borderEndEndRadius: 6, borderStartEndRadius: 6, height: 30, display: 'flex', alignItems: 'center', minWidth: 179, padding: '4px 11px' }}>
-                                                {param}
-                                            </Link>
-                                        </div> :
-                                    <>
-                                        <Button onClick={() => handleChangeMode(param, index)} type="text" size="small">
-                                            <span style={{ color: '#2F72FF' }}>
-                                                Изменить на операцию
-                                            </span>
-                                        </Button>
-                                        <Input
-                                            addonBefore={<Text>{operation.params[index]}</Text>}
-                                            type="text" 
-                                            value={param} 
-                                            onChange={(e) => handleInputChange(e.target.value, index)} 
-                                        />
-                                    </>
-                                }
-
-                            </div>
-                        ))}
-                    </div>
-                )}
-            </div>
-        </div>
-
+  function handleInputChange(value, paramIndex) {
+    const id = Number(nodeId || 0);
+    setParams((prev) =>
+      prev.map((param, index) => (index === paramIndex ? value : param))
     );
+    patchUserNodeByIndex(dispatch, id, {
+      params:
+        myNodes[id].params?.map((p, index) =>
+          index === paramIndex ? value : p
+        ) || [],
+    });
+  }
+
+  function handleChangeMode(param, paramIndex) {
+    const id = Number(nodeId || 0);
+    setIsDropped(false);
+    addUserNode(dispatch, { id: myNodes.length });
+    patchUserNodeByIndex(dispatch, id, {
+      params:
+        myNodes[id].params?.map((p, index) =>
+          index === paramIndex ? myNodes.length : p
+        ) || [], // сетим myNodes.length, как id следующей ноды
+    });
+    setOperation();
+    setParams([]);
+    navigate("/" + myNodes.length, { state: param }); // редиректим на страницу, param нужен для тайтла
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+      }}
+    >
+      <Text type="secondary">{operation?.name}</Text>
+      <div
+        ref={drop}
+        style={{
+          minHeight: "150px",
+          display: "flex",
+          flex: 1,
+          border: "2px dashed #d9d9d9",
+          borderRadius: "4px",
+          padding: "10px",
+          textAlign: "center",
+        }}
+      >
+        <Text type="secondary">
+          {!operation && "Перетаскивайте блоки для построения формулы"}
+        </Text>
+        {params?.length > 0 && operation && (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              gap: 20,
+              flexWrap: "wrap",
+            }}
+          >
+            {params.map((param, index) => (
+              <div
+                key={index}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: 5,
+                  alignItems: "start",
+                }}
+              >
+                {/* если параметр это реф на другую ноду, то отображаем кнопку, иначе это инпут */}
+                {typeof myNodes[Number(nodeId)]?.params?.[index] ===
+                "number" ? (
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <span
+                      style={{
+                        backgroundColor: "#FAFAFA",
+                        border: "1px solid #D9D9D9",
+                        padding: "0 11px",
+                        lineHeight: 1,
+                        borderStartStartRadius: 6,
+                        borderEndStartRadius: 6,
+                        height: 30,
+                        display: "flex",
+                        alignItems: "center",
+                        fontSize: 14,
+                      }}
+                    >
+                      x
+                    </span>
+                    <Link
+                      to={`/${myNodes[Number(nodeId)].params[index]}`}
+                      style={{
+                        borderLeft: "none",
+                        textDecoration: "none",
+                        border: "1px solid #D9D9D9",
+                        borderEndEndRadius: 6,
+                        borderStartEndRadius: 6,
+                        height: 30,
+                        display: "flex",
+                        alignItems: "center",
+                        minWidth: 179,
+                        padding: "4px 11px",
+                      }}
+                    >
+                      {param}
+                    </Link>
+                  </div>
+                ) : (
+                  <>
+                    <Button
+                      onClick={() => handleChangeMode(param, index)}
+                      type="text"
+                      size="small"
+                    >
+                      <span style={{ color: "#2F72FF" }}>
+                        Изменить на операцию
+                      </span>
+                    </Button>
+                    <Input
+                      addonBefore={<Text>{operation.params[index]}</Text>}
+                      type="text"
+                      value={param}
+                      onChange={(e) => handleInputChange(e.target.value, index)}
+                    />
+                  </>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+    </div>
+  );
 }
 
 EditArea.propTypes = {
-    nodeId: PropTypes.string,
-    operation: PropTypes.instanceOf(Operation),
-    setOperation: PropTypes.func.isRequired,
-    params: PropTypes.arrayOf(PropTypes.string).isRequired,
-    setParams: PropTypes.func,
+  nodeId: PropTypes.string,
+  operation: PropTypes.instanceOf(Operation),
+  setOperation: PropTypes.func.isRequired,
+  params: PropTypes.arrayOf(PropTypes.string).isRequired,
+  setParams: PropTypes.func,
 };
